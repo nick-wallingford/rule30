6bb851fac063c0a73f7551b05503c7754651b848
Nick Wallingford <nick@wallingford.cc>
2026-08-18 13:50:07 -0700

    simplify and/or complicate the avx reporting function

--- rule30_avx2.cpp
+++ rule30_avx2.cpp
@@ -6,19 +6,17 @@
 
 void report(__m256i x, uint64_t i) {
   std::array<uint32_t, 8> a;
+  x = _mm256_permutevar8x32_epi32(x, _mm256_set_epi32(0, 0, 0, 1, 3, 5, 7, 6));
   _mm256_storeu_si256(reinterpret_cast<__m256i *>(a.data()), x);
-  int n;
-  if (a[6] != 0xffff'ffff)
-    n = std::countr_one(a[6]);
-  else if (a[7] != 0xffff'ffff)
-    n = std::countr_one(a[7]) + 32;
-  else if (a[5] != 0xffff'ffff)
-    n = std::countr_one(a[5]) + 64;
-  else if (a[3] != 0xffff'ffff)
-    n = std::countr_one(a[3]) + 96;
-  else
-    n = std::countr_one(a[1]) + 128;
-  if (0 > printf("%4d%4d %08x %08x %08x %08x %08x\n", std::countr_one(i), n, a[1], a[3], a[5], a[7], a[6]))
+  int n = 0;
+  for (auto y : a)
+    if (y == 0xffff'ffff)
+      n += 32;
+    else {
+      n += std::countr_one(y);
+      break;
+    }
+  if (0 > printf("%4d%4d %08x %08x %08x %08x %08x\n", std::countr_one(i), n, a[4], a[3], a[2], a[1], a[0]))
     std::terminate();
 }
 
